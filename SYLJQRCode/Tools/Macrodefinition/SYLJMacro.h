6d1db136525f892bcda51cd70e678f0eebcdc315
jun.liu <jun.liu@travelzen.com>
2017-01-10 17:30:44 +0800

    调整工程结构

---
+++ SYLJQRCode/Tools/Macrodefinition/SYLJMacro.h
@@ -0,0 +1,15 @@
+//
+//  SYLJMacro.h
+//  SYLJQRCode
+//
+//  Created by 刘俊 on 2017/1/10.
+//  Copyright © 2017年 刘俊. All rights reserved.
+//
+
+#ifndef SYLJMacro_h
+#define SYLJMacro_h
+
+#define kScreenWidth ([UIScreen mainScreen].bounds.size.width)
+#define kScreenHeight ([UIScreen mainScreen].bounds.size.height)
+
+#endif /* SYLJMacro_h */
