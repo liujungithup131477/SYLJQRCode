32007aab6b364be607749948b5fef7f2d1606b69
jun.liu <jun.liu@travelzen.com>
2017-01-10 13:28:34 +0800

    添加 SYLJQRCodeViewController 核心类

---
+++ SYLJQRCode/SYLJQRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYLJQRCodeViewController.h
+//  SYLJQRCode
+//
+//  Created by 刘俊 on 2017/1/10.
+//  Copyright © 2017年 刘俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYLJQRCodeViewController : UIViewController
+
+@end
---
+++ SYLJQRCode/SYLJQRCodeViewController.m
@@ -0,0 +1,37 @@
+//
+//  SYLJQRCodeViewController.m
+//  SYLJQRCode
+//
+//  Created by 刘俊 on 2017/1/10.
+//  Copyright © 2017年 刘俊. All rights reserved.
+//
+
+#import "SYLJQRCodeViewController.h"
+
+@interface SYLJQRCodeViewController ()
+
+@end
+
+@implementation SYLJQRCodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
