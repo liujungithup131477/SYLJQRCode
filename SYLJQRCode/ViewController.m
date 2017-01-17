b2b2538a6ec53347ffa9e997ee3d9bc544c715ed
jun.liu <jun.liu@travelzen.com>
2017-01-17 15:04:23 +0800

    功能测试完整

--- SYLJQRCode/ViewController.m
+++ SYLJQRCode/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) UILabel *label;
+
 @end
 
 @implementation ViewController
@@ -18,20 +20,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-    [btn setTitle:@"扫一扫" forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    btn.frame = CGRectMake(100, 300, 100, 30);
-    btn.backgroundColor = [UIColor purpleColor];
-    [btn addTarget:self action:@selector(scan:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn];
+//    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+//    [btn setTitle:@"扫一扫" forState:UIControlStateNormal];
+//    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+//    btn.frame = CGRectMake(0, 0, 300, 300);
+//    btn.center = self.view.center;
+//    btn.backgroundColor = [UIColor purpleColor];
+//    [btn addTarget:self action:@selector(scan:) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:btn];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+    label.text = @"扫一扫";
+    label.backgroundColor = [UIColor purpleColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor whiteColor];
+    label.numberOfLines = 0;
+    label.userInteractionEnabled = YES;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scan:)];
+    [label addGestureRecognizer:tap];
+    label.center = self.view.center;
+    [self.view addSubview:label];
+    self.label = label;
 }
 
-- (void)scan:(UIButton *)sender
+- (void)scan:(UITapGestureRecognizer *)sender
 {
     SYLJQRCodeNavigationController *scanQRNaV = [SYLJQRCodeNavigationController scannerWithCompletion:^(NSString * _Nonnull stringValue) {
         NSLog(@"扫描结果: %@", stringValue);
-        [sender setTitle:stringValue forState:UIControlStateNormal];
+        self.label.text = stringValue;
     }];
     [self presentViewController:scanQRNaV animated:YES completion:NULL];
 }
