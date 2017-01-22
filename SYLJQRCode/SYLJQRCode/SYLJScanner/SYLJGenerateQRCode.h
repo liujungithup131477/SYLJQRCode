ee2b1fd9089c24051d0bec6ccc179dfe2fa9af51
jun.liu <jun.liu@travelzen.com>
2017-01-22 14:54:52 +0800

    生成二维码补充

--- SYLJQRCode/SYLJQRCode/SYLJScanner/SYLJGenerateQRCode.h
+++ SYLJQRCode/SYLJQRCode/SYLJScanner/SYLJGenerateQRCode.h
@@ -25,4 +25,14 @@
  */
 + (void)qrImageWithString:(NSString *)string avatar:(UIImage *)avatar completion:(void (^)(UIImage *))completion;
 
+/**
+ 使用 string / 头像 异步生成二维码图像，并且指定头像占二维码图像的比例
+
+ @param string 二维码图像的字符串
+ @param avatar 头像图像
+ @param scale 头像占二维码图像的比例
+ @param completion 完成回调
+ */
++ (void)qrImageWithString:(NSString *)string avatar:(UIImage *)avatar scale:(CGFloat)scale completion:(void (^)(UIImage *))completion;
+
 @end
